a0e202dccc69cc05f7f439372be095cbf21cb669
ceoDemitri <demichole@gmail.com>
2023-12-29 07:47:45 -0600

    first content and kickoff for site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>ArtSea</h1>
+<h2>Adventure off into a land like no other</h2>
+<p>
+	Welcome to a brandnew vision. Why wait on the world to create beautiful innovations. Lets step it
+	up.
+</p>
+
+<style>
+	* {
+		padding: 0;
+		margin: 0;
+	}
+</style>
