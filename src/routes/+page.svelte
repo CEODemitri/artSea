93aa0bf58b7068f7993a4157b4f52c1a062bd38b
ceoDemitri <demichole@gmail.com>
2024-01-05 00:05:49 -0600

    1hr build trying to add buttons.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,19 @@
-<h1 class="h1">ArtSea</h1>
+<h1 class="pb-3 pt-10 text-[52px]">ArtSea</h1>
 <h2>Adventure off into a land like no other</h2>
 <p>
 	Welcome to a brandnew vision. Why wait on the world to create beautiful innovations. Lets step it
-	up.
+	up. Introducing a fresh perspective.
 </p>
+<div class="p-5">
+	<a href="/" class="variant-filled-surface btn">
+		<span class="material-symbols-outlined"> door_open </span>
+		<span>Musuem</span>
+	</a>
+	<button type="button" class="variant-ghost btn">
+		<span class="material-symbols-outlined"> favorite </span>
+		<span>Collections</span>
+	</button>
+</div>
 
 <style>
-	* {
-		padding: 0;
-		margin: 0;
-	}
 </style>
